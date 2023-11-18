1a6a6aa71bcbffda647b9f3343f57684024a02f9
Birendra kumar <birendrabitian@gmail.com>
2023-11-18 22:55:41 +0530

    fixed all  inverted quotes &apos;

--- components/javascript/PureFunction.jsx
+++ components/javascript/PureFunction.jsx
@@ -5,7 +5,7 @@ function PureFunction() {
     <>
         <h1>Pure Functions in JavaScript</h1>
         <p>
-            A Pure Function is a type of function or code block that consistently produces the same output when provided with the same input, regardless of any changes in the program's state or data. Its result is solely determined by its input parameters and remains unaffected by the program's execution context.
+            A Pure Function is a type of function or code block that consistently produces the same output when provided with the same input, regardless of any changes in the program&apos;s state or data. Its result is solely determined by its input parameters and remains unaffected by the program&apos;s execution context.
         </p>
     </>
   )
--- components/sql/Like.jsx
+++ components/sql/Like.jsx
@@ -5,26 +5,26 @@ function Like() {
     <div>
       {/* query 1 */}
       <div className='text-cyan-200 text-lg bg-cyan-700 px-2 font-semibold border border-double my-2 italic'>
-        Sql query to show list of name starts with 'R'
+        Sql query to show list of name starts with &apos;R&apos;
         <div className='text-cyan-50 text-md'>
           select name,quantity,price
           <br/>from orders
-          <br/>where name like 'R%'
+          <br/>where name like &apos;R%&apos;
         </div>
         <hr/>
         Sql query to show product name having 4 characer only
         <div className='text-cyan-50 text-md'>
           select name,quantity,price
           <br/>from orders
-          <br/>where name like '_ _ _ _ '
+          <br/>where name like &apos;_ _ _ _ &apos;
           
         </div>
         <hr/>
-        Sql query to show product name having 5 characer only starts and ends with 'r'
+        Sql query to show product name having 5 characer only starts and ends with &apos;r&apos;
         <div className='text-cyan-50 text-md'>
           select name,quantity,price
           <br/>from orders
-          <br/>where name like 'r _ _ _ r'
+          <br/>where name like &apos;r _ _ _ r&apos;
           
         </div>
       </div>
